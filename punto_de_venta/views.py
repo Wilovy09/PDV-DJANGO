aa6e3bae40e965f42d04637d16a4d6edbd305ac9
Wilovy09 <SrWilovy09@gmail.com>
2023-10-29 21:56:08 -0600

    se agrega btn registrar usuario

--- punto_de_venta/views.py
+++ punto_de_venta/views.py
@@ -4,8 +4,22 @@ from .models import clientes
 from catalogo.models import Producto
 
 producto = Producto.objects.all()
+Clientes = clientes.objects.all()
 def punto_de_venta(request):
     if request.user.is_authenticated:
-        return render(request, 'punto_de_venta.html')
+        data = {
+            'form':ClienteForm(),
+            'clientess':Clientes,
+            'productos_lista':producto,
+            }
+        
+        if request.method == 'POST':
+            formulario = ClienteForm(data=request.POST)
+            if formulario.is_valid():
+                formulario.save()
+                data['mensaje'] = 'Cliente guardado'
+            else:
+                data['form'] = formulario
+        return render(request, 'punto_de_venta.html', data)
     else:
         return redirect('signin')
